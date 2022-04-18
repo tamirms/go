8643d8194c3aebedefd50a2e47a55ff944150d0f
Paul Bellamy <paul@stellar.org>
2022-04-18 17:06:19 +0100

    Add historyarchive.ArchiveBackend.Close()

--- historyarchive/archive.go
+++ historyarchive/archive.go
@@ -64,6 +64,7 @@ type ArchiveBackend interface {
 	PutFile(path string, in io.ReadCloser) error
 	ListFiles(path string) (chan string, chan error)
 	CanListFiles() bool
+	Close() error
 }
 
 type ArchiveInterface interface {
